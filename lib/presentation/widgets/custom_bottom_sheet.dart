71d7a6f64a68c044506f311126aa4c4e5bf25695
Aliaksandr Makhrachou <amakhrachou@ibagroup.eu>
2022-09-19 23:49:31 +0300

    Added share card method.

--- lib/presentation/widgets/custom_bottom_sheet.dart
+++ lib/presentation/widgets/custom_bottom_sheet.dart
@@ -5,6 +5,9 @@ import 'package:qr_flutter/qr_flutter.dart';
 import '../../blocs/share_card_bloc/share_card_bloc.dart';
 import 'change_card_method_bar.dart';
 
+import 'package:share_plus/share_plus.dart';
+
+
 class CustomBottomSheet extends StatelessWidget {
 
   final String qrLink;
@@ -22,20 +25,24 @@ class CustomBottomSheet extends StatelessWidget {
             children: [
               Column(
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      IconButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          splashRadius: 20,
-                          icon: Icon(Icons.close, color: Colors.white, size: 30)),
-                      Center(
-                          child: Text('My Card',
-                              style: TextStyle(color: Colors.white, fontSize: 18))),
-                      SizedBox(width: 35)
-                    ],
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                          padding: EdgeInsets.zero,
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            splashRadius: 20,
+                            icon: Icon(Icons.close, color: Colors.white, size: 40)),
+                        Center(
+                            child: Text('My Card',
+                                style: TextStyle(color: Colors.white, fontSize: 25))),
+                        SizedBox(width: 40)
+                      ],
+                    ),
                   ),
                   Container(
                       height: 250,
@@ -59,22 +66,55 @@ class CustomBottomSheet extends StatelessWidget {
                         style: TextStyle(
                             color: Colors.white,
                             fontWeight: FontWeight.bold,
-                            fontSize: 18),
+                            fontSize: 20),
                         textAlign: TextAlign.center),
                   ),
                 ],
               ),
 
+              GestureDetector(
+                onTap: () {
+                  Share.share('You can find my BCard following the link: $qrLink');
+                },
+                child: Container(
+                  padding: EdgeInsets.all(15.0),
+                  margin: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 50.0),
+                  height: 70,
+                  decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(15)),
+                  child: Row(
+                    children: [
+                      Icon(Icons.ios_share_rounded, color: Colors.white, size: 30),
+                      Expanded(
+                        child: Center(
+                          child: Text("Share",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                  fontSize: 25)),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 30
+                      )
+                    ],
+                  ),
+                ),
+              ),
 
 
-              if (state is ShareCardByScanState)
-                ChangeCardMethodBar(shareMethodName: 'Scan'),
-              if (state is ShareCardByTextState)
-                ChangeCardMethodBar(shareMethodName: 'Text'),
-              if (state is ShareCardByEmailState)
-                ChangeCardMethodBar(shareMethodName: 'Email'),
-              if (state is ShareCardByCopyState)
-                ChangeCardMethodBar(shareMethodName: 'Copy')
+              //===================================================
+              //OLD UI OF THE ORIGINAL APP
+              //===================================================
+              // if (state is ShareCardByScanState)
+              //   ChangeCardMethodBar(shareMethodName: 'Scan'),
+              // if (state is ShareCardByTextState)
+              //   ChangeCardMethodBar(shareMethodName: 'Text'),
+              // if (state is ShareCardByEmailState)
+              //   ChangeCardMethodBar(shareMethodName: 'Email'),
+              // if (state is ShareCardByCopyState)
+              //   ChangeCardMethodBar(shareMethodName: 'Copy')
             ],
           );
         },
