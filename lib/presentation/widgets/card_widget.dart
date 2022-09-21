c3464641a21633ec7349dce9a38de1cb752dd8fb
Aliaksandr Makhrachou <amakhrachou@ibagroup.eu>
2022-09-21 23:34:01 +0300

    Added uri_launcher.

--- lib/presentation/widgets/card_widget.dart
+++ lib/presentation/widgets/card_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 import '../../blocs/card_page_bloc/card_page_bloc.dart';
 import '../../data/models/card_model.dart';
@@ -217,18 +219,56 @@ class ExtraTextWidget extends StatelessWidget {
       required this.icon})
       : super(key: key);
 
+  Future<void> onTap() async {
+    if (label == 'Phone Number') {
+      final Uri launchUri = Uri(scheme: 'tel', path: value);
+
+      if (await canLaunchUrl(launchUri)) {
+        await launchUrl(launchUri);
+      }
+    } else if (label == 'Email') {
+      final Uri emailUri = Uri(scheme: 'mailto', path: value);
+
+      if (await canLaunchUrl(emailUri)) {
+        await launchUrl(emailUri);
+      }
+    } else if (label == 'Telegram') {
+      String url = value;
+
+      if (!value.contains('http')) {
+        url = 'https://t.me/${value.replaceFirst('@', '')}';
+        if (await canLaunchUrlString(url)) {
+          await launchUrlString(url, mode: LaunchMode.externalApplication);
+        }
+      } else {
+        if (await canLaunchUrlString(url)) {
+          await launchUrlString(url, mode: LaunchMode.externalApplication);
+        }
+      }
+    } else {
+      if (await canLaunchUrlString(value)) {
+        await launchUrlString(value);
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        CircleAvatar(radius: 20, backgroundColor: Color(color), child: icon),
-        Expanded(
-          child: ListTile(
-            title: Text(value, style: TextStyle(fontWeight: FontWeight.bold)),
-            subtitle: Text(label),
-          ),
-        )
-      ],
+    return GestureDetector(
+      onTap: () async {
+        await onTap();
+      },
+      child: Row(
+        children: [
+          CircleAvatar(radius: 20, backgroundColor: Color(color), child: icon),
+          Expanded(
+            child: ListTile(
+              title: Text(value, style: TextStyle(fontWeight: FontWeight.bold)),
+              subtitle: Text(label),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
