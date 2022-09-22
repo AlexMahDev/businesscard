eb360cbbea89745cc843b2686fdb1abf423747e4
Aliaksandr Makhrachou <amakhrachou@ibagroup.eu>
2022-09-22 18:12:40 +0300

    Set width and added regex to AddContactByLink AlertDialog.

--- lib/presentation/contacts_page.dart
+++ lib/presentation/contacts_page.dart
@@ -8,6 +8,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../blocs/contact_bloc/contact_bloc.dart';
 import '../data/models/card_model.dart';
+import '../data/models/contact_model.dart';
 import 'contact_info_page.dart';
 
 class ContactsPage extends StatefulWidget {
@@ -61,40 +62,7 @@ class _ContactsPageState extends State<ContactsPage> {
                         onPressed: () {
                           showDialog(
                               context: context,
-                              builder: (ctx) => AlertDialog(
-                                    title: Text("Add contact"),
-                                    content: SizedBox(
-                                      height: 120,
-                                      child: Column(
-                                        children: [
-                                          Text(
-                                              "Enter link you received from contact"),
-                                          SizedBox(
-                                            height: 10,
-                                          ),
-                                          CustomTextField(
-                                              controller: urlController,
-                                              hintText: 'Link'),
-                                        ],
-                                      ),
-                                    ),
-                                    actions: <Widget>[
-                                      TextButton(
-                                          onPressed: () {
-                                            Navigator.pop(context);
-                                            BlocProvider.of<ContactBloc>(context)
-                                                .add(SaveContactManualEvent(urlController.text, state.contacts));
-                                          },
-                                          child: Text('Add',
-                                              style: TextStyle(fontSize: 18))),
-                                      TextButton(
-                                          onPressed: () {
-                                            Navigator.pop(context);
-                                          },
-                                          child: Text('Cancel',
-                                              style: TextStyle(fontSize: 18))),
-                                    ],
-                                  ));
+                              builder: (ctx) => AddContactByLinkWidget(contacts: state.contacts));
                         },
                         icon: Icon(Icons.add));
 
@@ -258,19 +226,88 @@ class _ContactsPageState extends State<ContactsPage> {
   }
 }
 
-class AddContactByLinkWidget extends StatelessWidget {
-  const AddContactByLinkWidget({Key? key}) : super(key: key);
+class AddContactByLinkWidget extends StatefulWidget {
+
+  final List<ContactModel> contacts;
+
+  const AddContactByLinkWidget({Key? key, required this.contacts}) : super(key: key);
+
+  @override
+  State<AddContactByLinkWidget> createState() => _AddContactByLinkWidgetState();
+}
+
+class _AddContactByLinkWidgetState extends State<AddContactByLinkWidget> {
+
+  late final TextEditingController urlController;
+  final _validation = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    urlController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    urlController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      decoration: BoxDecoration(
-          color: Colors.white.withOpacity(0.9),
-          borderRadius: BorderRadius.all(Radius.circular(10))),
+    return Form(
+      key: _validation,
+      child: AlertDialog(
+        title: Text("Add contact"),
+        content: Container(
+          width: 400,
+          //constraints: BoxConstraints(minWidth: 500, maxWidth: 600),
+          height: 120,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                  "Enter link you received from contact"),
+              SizedBox(
+                height: 10,
+              ),
+              Expanded(
+                child: CustomTextField(controller: urlController, hintText: "Link", validator: (text) {
+                  if(text == '') {
+                    return "URL is required";
+                  } else if (!RegExp(r"^https:\/\/alexmahdev\.page\.link\/\b").hasMatch(text!)) {
+                    return "Enter valid URL";
+                  }
+                  return null;
+                }),
+              ),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          TextButton(
+              onPressed: () {
+                if(_validation.currentState!.validate()) {
+                  Navigator.pop(context);
+                  BlocProvider.of<ContactBloc>(context)
+                      .add(SaveContactManualEvent(urlController.text, widget.contacts));
+                }
+              },
+              child: Text('Add',
+                  style: TextStyle(fontSize: 18))),
+          TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Cancel',
+                  style: TextStyle(fontSize: 18))),
+        ],
+      ),
     );
   }
 }
+
+
 
 class ContactWidget extends StatelessWidget {
   final CardModel card;
