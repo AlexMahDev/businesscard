ddb1268523d13252983cb4c68386012bbb2b7035
Aliaksandr Makhrachou <amakhrachou@ibagroup.eu>
2022-10-24 03:09:54 +0300

    Added get_it package (2 commit).

--- lib/presentation/blocs/auth_bloc/auth_bloc.dart
+++ lib/presentation/blocs/auth_bloc/auth_bloc.dart
@@ -3,17 +3,18 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter/cupertino.dart';
 
 import '../../../data/repositories/auth_repository.dart';
+import '../../../setupInjection.dart';
 
 part 'auth_event.dart';
 part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  final AuthRepository authRepository;
-  AuthBloc({required this.authRepository}) : super(UnAuthenticated()) {
+
+  AuthBloc() : super(UnAuthenticated()) {
     on<SignInRequested>((event, emit) async {
       emit(Loading());
       try {
-        await authRepository.signIn(
+        await getIt<AuthRepository>().signIn(
             email: event.email, password: event.password);
         emit(Authenticated());
       } catch (e) {
@@ -24,7 +25,7 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
     on<SignUpRequested>((event, emit) async {
       emit(Loading());
       try {
-        await authRepository.signUp(
+        await getIt<AuthRepository>().signUp(
             email: event.email, password: event.password);
         emit(Authenticated());
       } catch (e) {
@@ -34,7 +35,7 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
     });
     on<SignOutRequested>((event, emit) async {
       emit(Loading());
-      await authRepository.signOut();
+      await getIt<AuthRepository>().signOut();
       emit(UnAuthenticated());
     });
   }
